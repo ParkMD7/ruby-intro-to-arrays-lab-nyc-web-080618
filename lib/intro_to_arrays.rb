551fd653822ef1f0dbb43f5a7cbe88ce6a1a2c79
Parker Dinsmore <parkermdinsmore@gmail.com>
2018-07-17 04:50:07 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -23,12 +23,12 @@ def last_element(taylor_swift)
 end
 
 
-def first_element_with_array_methods()
+def first_element_with_array_methods(south_east_asia)
   
 end
 
 
-def last_element_with_array_methods()
+def last_element_with_array_methods(south_east_asia)
 
 end
 
