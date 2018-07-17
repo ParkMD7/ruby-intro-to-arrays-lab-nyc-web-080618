f8e475886396efcb6f79ad72a0afb90e55425b88
Parker Dinsmore <parkermdinsmore@gmail.com>
2018-07-17 04:51:00 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -24,15 +24,15 @@ end
 
 
 def first_element_with_array_methods(south_east_asia)
-  
+  south_east_asia.first
 end
 
 
 def last_element_with_array_methods(south_east_asia)
-
+  south_east_asia.last
 end
 
 
-def length_of_array()
-  
+def length_of_array(programming_languages)
+  programming_languages.length
 end
